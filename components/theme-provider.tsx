3173a873717e6d33edb6de84c9d9ef5aacf16927
Viet An <luongvietan.231123@gmail.com>
2025-06-06 14:02:55 +0700

    Enhance type safety in ThemeProvider component by defining a more specific attribute type. Update ThemeProviderProps interface to accept a union of attribute types for better flexibility in theme management.

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,10 +2,13 @@
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
+// Định nghĩa kiểu Attribute từ next-themes
+type Attribute = "class" | "data-theme" | "data-mode";
+
 // Định nghĩa kiểu ThemeProviderProps đơn giản
 interface ThemeProviderProps {
   children: React.ReactNode;
-  attribute?: string;
+  attribute?: Attribute | Attribute[];
   defaultTheme?: string;
   enableSystem?: boolean;
   disableTransitionOnChange?: boolean;
